6662d289009133a637b0e848105601cfa72a307f
Gertje823 <36937387+Gertje823@users.noreply.github.com>
2021-01-19 21:16:29 +0100

    Fixed data retrieval
    You can now download subs again!

--- Videoland-Sub-Downloader.py
+++ Videoland-Sub-Downloader.py
@@ -1,13 +1,12 @@
-import requests
+import requests, re, json, time, os
 import urllib.request
-import time
 from bs4 import BeautifulSoup
 from vtt_to_srt.__main__ import vtt_to_srt
-import os
 
 cookie = {'vlId': 'YOUR_vlId_HERE'}
 
 url = input("Wat is de Videoland url?")
+url_pattern = "(https?:\/\/(?:www\.|(?!www)))videoland.com\/series\/(.*?)\/(.*?)\/\\\?(.*)"
 if "/films/" in url:
     data = requests.get(url)
     soup = BeautifulSoup(data.text, "html.parser")
@@ -25,7 +24,7 @@ if "/films/" in url:
     link = f"https://www.videoland.com/api/v3/subtitles/{x[4]}"
     r = requests.get(link, cookies=cookie)
     if r.status_code == 404:
-        print("Sorry, er is geen ondertiteling gevonden voor deze aflevering")
+        print("Sorry, er is geen ondertiteling gevonden voor deze film")
     else:
         print(f"{filename} aan het downloaden")
         open(filename, 'wb').write(r.content)
@@ -34,43 +33,61 @@ if "/films/" in url:
         os.remove(filename)
 
 elif "/series/" in url:
-    
-    Season2 = input("Welk seizoen? (bv. 3)")
-    Season = "{0:0=2d}".format(int(Season2))
+    link = re.search(url_pattern, url).group(4)
+    try:
+        url= url.replace(link,"")
+    except:
+        pass
+    wanted_season = input("Welk seizoen? (bv. 3)")
+    Season = "{0:0=2d}".format(int(wanted_season))
     Ep = 0
-    data = requests.get(url)
+    data = requests.get(url, cookies=cookie)
     soup = BeautifulSoup(data.text, "html.parser")
     title = soup.select('h1')[0].text.strip()
-    title = title.replace(" ",".")
-    for link in soup.find_all("a"):
-        text= link.text
-        text = text.replace("Seizoen ","")
-        if text == Season2:
-            url = "https://www.videoland.com/"+link.get('href')
-            data = requests.get(url)
+    title = title.replace(" ", ".")
+
+    # Get json with serie data
+
+    pattern = "window.__INITIAL_STATE__ =(.*?);</script><script>"
+    json_data = re.search(pattern, data.text).group(1)
+    data = json.loads(json_data)
+    season_num = 0
+
+    # Check for wanted season
+    for season in data["videos"]["details"]["S500175"]["refs"]:
+        if season_num == int(wanted_season)-1:
+            season = season.replace("SN", "")
+            path = data["application"]["path"]["current"]
+            link = f"https://www.videoland.com{path}/{season}"
+            data = requests.get(link, cookies=cookie)
             soup = BeautifulSoup(data.text, "html.parser")
-            for link in soup.find_all("div", {"class": "episode"}):  
-                for link in link.find_all('a'):
-                    link = link.get('href')
-                    link = link.replace("/player/","")
-                    link = link.split("/")
-                    link = f"https://www.videoland.com/api/v3/subtitles/{link[0]}"
-                    Ep+=1
-                    Ep= "{0:0=2d}".format(Ep)
+            pattern = "window.__INITIAL_STATE__ =(.*?);</script><script>"
+            json_data = re.search(pattern, data.text).group(1)
+            data = json.loads(json_data)
+            # Get ids of episodes in season
+            for episodes in data["videos"]["details"].items():
+                if episodes[1]["type"] == "episode":
+                    id = episodes[1]["id"]
+                    link = f"https://www.videoland.com/api/v3/subtitles/{id}"
+                    Ep = episodes[1]["position"]
+                    Ep = "{0:0=2d}".format(Ep)
                     filename = f"{title}.S{Season}E{Ep}.vtt"
-                    filename = filename.replace(":","")
-                    Ep= int(Ep)
+                    filename = filename.replace(":", "")
+                    Ep = int(Ep)
                     r = requests.get(link, cookies=cookie)
+                    # Check if sub is available
                     if r.status_code == 404:
                         print("Sorry, er is geen ondertiteling gevonden voor deze aflevering")
                     else:
+                        # Download sub
                         print(f"{filename} aan het downloaden")
                         open(filename, 'wb').write(r.content)
                         print("Converting to srt")
                         vtt_to_srt(filename)
                         os.remove(filename)
-        else:
-            pass
+                    print()
+
+        season_num = +1
 
 else:
     print("Fout in URL. Probeer het opnieuw.")
